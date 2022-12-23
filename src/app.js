7e32dcdb25a59f7691474e0af4a97b3ff9529b19
Rifat <rifat@avtoalfa.com>
2022-12-23 15:16:02 +0300

    MRR 2022.12.23 | created first version of ruler

--- src/app.js
+++ src/app.js
@@ -14,15 +14,16 @@ import Stats from "three/addons/stats.module.js";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 import { createBox } from "./utils.js";
 
-import Raycasting from "./raycaster.js";
+import cast from "./raycaster.js";
 import UI from "./ui.js";
 
 let cameraPersp, currentCamera;
 let scene, renderer, orbit;
 let rulerEnabled, labelRenderer, stats;
+const boxes = [];
 
 init();
-render();
+// render();
 
 function init() {
   const aspect = window.innerWidth / window.innerHeight;
@@ -45,18 +46,26 @@ function init() {
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
 
-  const texture = new THREE.TextureLoader().load("textures/crate.gif", render);
+  const texture = new THREE.TextureLoader().load("textures/crate.gif");
   texture.anisotropy = renderer.capabilities.getMaxAnisotropy();
 
   const cubeGeometry = new THREE.BoxGeometry(200, 200, 200);
+  // const cubeMaterial = new THREE.MeshBasicMaterial( { color: 0xff0000} );
 
   const cubeMaterial = new THREE.MeshLambertMaterial({
     map: texture,
     transparent: true
   });
+  // Если материал перекрывает точки, то указываем такие значения
+  cubeMaterial.polygonOffset = true;
+  cubeMaterial.polygonOffsetUnits = 1;
+  cubeMaterial.polygonOffsetFactor = 1;
 
   const sphereGeometry = new THREE.SphereGeometry(100, 32, 16);
-  const sphereMaterial = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
+  const sphereMaterial = new THREE.MeshBasicMaterial( { color: 0xcccc00} );
+  sphereMaterial.polygonOffset = true;
+  sphereMaterial.polygonOffsetUnits = 1;
+  sphereMaterial.polygonOffsetFactor = 1;
 
   const loader = new GLTFLoader();
   let car = null;
@@ -69,9 +78,10 @@ function init() {
     car.position.x = -290;
     car.position.y = 0;
     car.position.z = -320;
+    boxes.push(createBox(car));
 
     scene.add(car);
-    render();
+    // render();
   }, function (xhr) {
   }, function (error) {
       console.log("error")
@@ -80,7 +90,7 @@ function init() {
   orbit = new OrbitControls(currentCamera, renderer.domElement);
   orbit.enableDamping = true;
   orbit.update();
-  orbit.addEventListener("change", render);
+  // orbit.addEventListener("change", render);
 
   const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial);
   cubeMesh.position.z = 200;
@@ -102,31 +112,22 @@ function init() {
   sphereMesh.position.x = 200;
   // Добавляем атрибут interactive "true" элементам, которые хотим передвигать
   sphereMesh.userData.interactive = true;
+
   // Добавляем атрибут scale для того, чтобы флип мог работать
   cubeMesh.userData.scale = Object.assign({}, cubeMesh.scale);
   scene.add(sphereMesh);
 
+  const planeGeometry = new THREE.PlaneGeometry(2000, 2000, 8, 8);
+  const planeMaterial = new THREE.MeshBasicMaterial({ color: 0xff00ff, side: THREE.DoubleSide });
+  planeMaterial.polygonOffset = true;
+  planeMaterial.polygonOffsetUnits = 1;
+  planeMaterial.polygonOffsetFactor = 1;
+  const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+  plane.rotateX( - Math.PI / 2);
+  plane.position.setY(-100);
+  scene.add(plane);
+
   window.addEventListener("resize", onWindowResize);
-
-  // Передаем в в элементы управления сцену, камеру, рендерер
-
-  let clickedObject = null;
-
-  Raycasting(currentCamera, scene, () => {
-    const boxes = [];
-    boxes.push(createBox(car));
-
-    return boxes;
-  }, 
-  (intersation) => {
-    if (intersation) {
-      clickedObject = intersation;
-      return;
-    }
-    clickedObject = null;
-    
-    render();
-  });
 
   // RULER
   UI.on("rulerEnabled", () => {
@@ -137,7 +138,11 @@ function init() {
   UI.on("rulerCanceled", () => {
     orbit.enabled = true;
     rulerEnabled = false;
-    resetLine();
+    if (pointsCache.length > 1) {
+      finishLine();
+    } else {
+      resetLine();
+    }
   });
 
   let lineId = 0,
@@ -145,6 +150,17 @@ function init() {
     drawingLine = false,
     measurementLabels = {},
     mouse = new THREE.Vector2();
+
+  const lines = [];
+  // let line = [];
+  let pointsCache = [];
+  let labelsCache = [];
+  let linesCache = [];
+  let intersects = null;
+  // let drawingLine = false;
+  let mousePrev = [];
+  let dragging = false;
+  // let mouse = new THREE.Vector2();
   
   labelRenderer = new CSS2DRenderer();
   labelRenderer.setSize(window.innerWidth, window.innerHeight);
@@ -153,30 +169,63 @@ function init() {
   labelRenderer.domElement.style.pointerEvents = "none";
   document.body.appendChild(labelRenderer.domElement);
 
-  window.addEventListener('keyup', function (event) {
-    if (rulerEnabled) {
-      if (drawingLine) {
-        resetLine();
-      }
-    }
-  });
+  // window.addEventListener('keyup', function (event) {
+  //   if (rulerEnabled) {
+  //     if (drawingLine) {
+  //       resetLine();
+  //     }
+  //   }
+  // });
 
   function resetLine() {
     scene.remove(line);
     scene.remove(measurementLabels[lineId]);
+    scene.remove(pointsCache.at(0));
+    scene.remove(labelsCache.at(0));
     drawingLine = false;
+    pointsCache = [];
+    labelsCache = [];
+    linesCache = [];
   }
 
-  renderer.domElement.addEventListener('pointerdown', onClick, false);
-  function onClick() {
-    if (!rulerEnabled || !clickedObject) {
+  function finishLine() {
+    const positions = line.geometry.attributes.position.array;
+    positions[3] = intersects.point.x;
+    positions[4] = intersects.point.y;
+    positions[5] = intersects.point.z;
+    line.geometry.attributes.position.needsUpdate = true;
+    lineId++;
+    drawingLine = false;
+    if (linesCache.length + 1 !== pointsCache.length) {
+      scene.remove(linesCache.at(linesCache.length - 1));
+      scene.remove(labelsCache.at(labelsCache.length - 1));
+    }
+    pointsCache = [];
+    labelsCache = [];
+    linesCache = [];
+  }
+
+  renderer.domElement.addEventListener('mouseup', onClick, false);
+  function onClick(event) {
+    console.log();
+    if (dragging || event.which !== 1 || !rulerEnabled) {
       return;
     }
-    console.log(clickedObject);
-    if (rulerEnabled) {
+    // if (!rulerEnabled && !clickedObject) {
+    //   return;
+    // }
+    // if (!drawingLine) {
+      intersects = cast(mouse, currentCamera, scene, boxes);
+      const dotGeometry = new THREE.SphereGeometry(5, 32, 16);
+      const dotMaterial = new THREE.MeshBasicMaterial( { color: 0xffffff } );
+      const dot = new THREE.Mesh(dotGeometry, dotMaterial);
+      dot.position.copy(intersects.point);
+      scene.add(dot);
+      pointsCache.push(dot);
+
       const points = [];
-      points.push(clickedObject.point);
-      points.push(clickedObject.point.clone());
+      points.push(intersects.point);
+      points.push(intersects.point.clone());
       const geometry = new THREE.BufferGeometry().setFromPoints(
         points
       );
@@ -191,7 +240,8 @@ function init() {
         })
       );
       line.frustumCulled = false;
-      scene.add(line);
+      linesCache.push(line);
+      scene.add(line)
 
       const measurementDiv = document.createElement(
         'div'
@@ -199,52 +249,84 @@ function init() {
       measurementDiv.className = 'measurementLabel';
       measurementDiv.innerText = '0.0m';
       const measurementLabel = new CSS2DObject(measurementDiv);
-      measurementLabel.position.copy(clickedObject.point);
+      measurementLabel.position.copy(intersects.point);
       measurementLabels[lineId] = measurementLabel;
+      labelsCache.push(measurementLabel);
       scene.add(measurementLabels[lineId]);
       drawingLine = true;
-    } else {
-      const positions = line.geometry.attributes.position.array;
-      positions[3] = clickedObject.point.x;
-      positions[4] = clickedObject.point.y;
-      positions[5] = clickedObject.point.z;
-      line.geometry.attributes.position.needsUpdate = true;
-      lineId++;
-      drawingLine = false;
-    }
+    // }
+
+  }
+
+  document.addEventListener('mousedown', onDocumentMouseDown, false);
+  function onDocumentMouseDown() {
+    dragging = false;
   }
 
   document.addEventListener('mousemove', onDocumentMouseMove, false);
   function onDocumentMouseMove(event) {
-    event.preventDefault();
-
     mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
     mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
-
+    if (mouse.x !== mousePrev[0] && mouse.y !== mousePrev[1]) {
+      mousePrev = [mouse.x, mouse.y];
+      dragging = true;
+    }
     if (drawingLine) {
-      if (clickedObject) {
+      intersects = cast(mouse, currentCamera, scene, boxes);
+      if (intersects) {
         const positions = line.geometry.attributes.position.array;
         const v0 = new THREE.Vector3(
             positions[0],
             positions[1],
-            positions[2],
-        );
+            positions[2]
+        )
         const v1 = new THREE.Vector3(
-            clickedObject.point.x,
-            clickedObject.point.y,
-            clickedObject.point.z,
-        );
-        positions[3] = clickedObject.point.x;
-        positions[4] = clickedObject.point.y;
-        positions[5] = clickedObject.point.z;
-        line.geometry.attributes.position.needsUpdate = true;
-        const distance = v0.distanceTo(v1);
+            intersects.point.x,
+            intersects.point.y,
+            intersects.point.z
+        )
+        positions[3] = intersects.point.x
+        positions[4] = intersects.point.y
+        positions[5] = intersects.point.z
+        line.geometry.attributes.position.needsUpdate = true
+        const distance = v0.distanceTo(v1)
         measurementLabels[lineId].element.innerText =
-            distance.toFixed(2) + 'm';
-        measurementLabels[lineId].position.lerpVectors(v0, v1, 0.5);
+            distance.toFixed(2) + 'm'
+        measurementLabels[lineId].position.lerpVectors(v0, v1, 0.5)
       }
     }
   }
+  // document.addEventListener('mousemove', onDocumentMouseMove, false);
+  // function onDocumentMouseMove(event) {
+  //   event.preventDefault();
+
+  //   mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  //   mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+  //   if (drawingLine) {
+  //     if (clickedObject) {
+  //       const positions = line.geometry.attributes.position.array;
+  //       const v0 = new THREE.Vector3(
+  //           positions[0],
+  //           positions[1],
+  //           positions[2],
+  //       );
+  //       const v1 = new THREE.Vector3(
+  //           clickedObject.point.x,
+  //           clickedObject.point.y,
+  //           clickedObject.point.z,
+  //       );
+  //       positions[3] = clickedObject.point.x;
+  //       positions[4] = clickedObject.point.y;
+  //       positions[5] = clickedObject.point.z;
+  //       line.geometry.attributes.position.needsUpdate = true;
+  //       const distance = v0.distanceTo(v1);
+  //       measurementLabels[lineId].element.innerText =
+  //           distance.toFixed(2) + 'm';
+  //       measurementLabels[lineId].position.lerpVectors(v0, v1, 0.5);
+  //     }
+  //   }
+  // }
 
   stats = Stats();
   document.body.appendChild(stats.dom);
@@ -262,20 +344,21 @@ function onWindowResize() {
 
   renderer.setSize(window.innerWidth, window.innerHeight);
 
-  render();
+  // render();
 
 }
 
 function render() {
   renderer.render(scene, currentCamera);
   stats.update();
-  labelRenderer.render(scene, currentCamera);
 }
 
 function animate() {
   requestAnimationFrame(animate);
-  render();
+  renderer.render(scene, currentCamera);
+  orbit.update();
   stats.update();
+  labelRenderer.render(scene, currentCamera);
 }
 
 animate();
--- src/raycaster.js
+++ src/raycaster.js
@@ -1,48 +1,20 @@
 import * as THREE from 'three';
 
 const raycaster = new THREE.Raycaster();
-const clickMouse = new THREE.Vector2();
-let dragging = false;
-let mousePrev = [];
 
-export default function Raycasting(camera, scene, getBoxes, callback, returnIntersactionPoint) {
-  window.addEventListener("mousedown", (event) => {
-    dragging = false;
-  });
+export default function cast(mouse, camera, scene, boxes) {
+  raycaster.setFromCamera(mouse, camera);
+  let intersaction = raycaster.intersectObjects(scene.children);
+  let model = intersaction.filter((item) => item.object.userData.interactive)[0]?.object;
 
-  window.addEventListener("mousemove", (event) => {
-    if (event.clientX !== mousePrev[0] && event.clientY !== mousePrev[1]) {
-      dragging = true;
-      mousePrev = [event.clientX, event.clientY];
-    }
-  });
-  
-  window.addEventListener("mouseup", (event) => {
-    if (dragging) {
-      return;
-    }
-
-    clickMouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-    clickMouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
-
-    raycaster.setFromCamera(clickMouse, camera);
-    let intersation = raycaster.intersectObjects(scene.children);
-    let model = found.filter((item) => item.object.userData.interactive)[0]?.object;
-    // console.log(raycaster.intersectObjects(scene.children));
-
-    if (!intersation) {
-      const boxes = getBoxes();
-      for (let box of boxes) {
-        if (raycaster.ray.intersectsBox(box)) {
-          model = box.model;
-          intersation = raycaster.intersectObjects(box.model);
-          // console.log(box)
-          // intersation.userData.parent = box;
-          // console.log(intersation)
-        }
+  if (!intersaction) {
+    for (let box of boxes) {
+      if (raycaster.ray.intersectsBox(box)) {
+        model = box.model;
+        intersaction = raycaster.intersectObjects(box.model);
       }
     }
+  }
 
-    callback(returnIntersactionPoint ? intersation[0] : model);
-  });
+  return intersaction[0];
 }
